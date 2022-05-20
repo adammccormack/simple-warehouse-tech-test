530aaa7ea0fcc0b946207b13ae47c04c700b4974
Adam <adam.j.mccor@gmail.com>
2022-05-20 12:18:24 +0100

    1, init method creates warehouse and passed spec

--- spec/simple_warehouse_spec.rb
+++ spec/simple_warehouse_spec.rb
@@ -5,12 +5,15 @@ describe SimpleWarehouse do
   it { is_expected.to respond_to(:init).with(2).argument }
 
   describe '#init' do
+    let (:output) { [[1, 2 ],
+                     [3, 4 ]]
+   }
     it 'initialises a new warehouse' do
       warehouse = SimpleWarehouse.new
+      
+      result = warehouse.init(2,2)
 
-      warehouse.init(10,10)
-
-      expect(warehouse.view).to be_truthy
+      expect(result).to match_array(output)
     end
   end
 end
