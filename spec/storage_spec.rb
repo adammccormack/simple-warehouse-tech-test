ff7e8aa5dc3b0c1f9522917f01ca4edcdd369a04
Adam <adam.j.mccor@gmail.com>
2022-05-24 20:14:58 +0100

    1, added storage remove spec

--- spec/storage_spec.rb
+++ spec/storage_spec.rb
@@ -16,4 +16,19 @@ describe Storage do
       expect(warehouse.view).to match_array(output)
     end
   end
+
+  describe '#remove_product_at_location' do
+    let(:output) {
+      [[' ', ' '],
+       [' ', ' ']]
+    }
+    it 'stores the product at given coordinates' do
+      warehouse.init(2,2)
+      warehouse.store(1,1,1,1,'P')
+
+      warehouse.remove(1,1,1,1)
+
+      expect(warehouse.view).to match_array(output)
+    end
+  end
 end
